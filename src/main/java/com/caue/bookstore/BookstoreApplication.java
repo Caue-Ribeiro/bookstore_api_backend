929648e5473b5c89f782e5a37f5cf8d3a391da43
Cauê Ribeiro <caueribeiro.dev@gmail.com>
2026-05-11 17:18:47 -0300

    add RestClient configuration for external API integration

--- src/main/java/com/caue/bookstore/BookstoreApplication.java
+++ src/main/java/com/caue/bookstore/BookstoreApplication.java
@@ -3,11 +3,24 @@ package com.caue.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
+    @Configuration
+    public class RestClientConfig {
+
+        @Bean
+        public RestClient.Builder restClientBuilder() {
+            return RestClient.builder();
+        }
+    }
 
 }
