b2a7d81f4bf90fc8d1f424f5f885778d1ffaef09
Cauê Ribeiro <caueribeiro.dev@gmail.com>
2026-05-11 17:18:11 -0300

    add pagination to getAllBooks endpoint and implement deleteBookById method in BookService

--- src/main/java/com/caue/bookstore/controllers/BookController.java
+++ src/main/java/com/caue/bookstore/controllers/BookController.java
@@ -3,38 +3,59 @@ package com.caue.bookstore.controllers;
 import com.caue.bookstore.dto.BookRequestDTO;
 import com.caue.bookstore.dto.BookResponseDTO;
 import com.caue.bookstore.services.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value= "api/books")
+@RequestMapping(value = "api/books")
 public class BookController {
 
     private final BookService service;
 
+
     public BookController(BookService service) {
         this.service = service;
+
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<BookResponseDTO> getBookById(@PathVariable UUID id){
+    public ResponseEntity<BookResponseDTO> getBookById(@PathVariable UUID id) {
 
         BookResponseDTO book = service.getBookById(id);
 
         return ResponseEntity.ok(book);
     }
 
+
+    @GetMapping
+    ResponseEntity<Page<BookResponseDTO>> getAllBooks(Pageable pageable) {
+        Page<BookResponseDTO> books = service.getAllBooks(pageable);
+
+        return ResponseEntity.ok(books);
+    }
+
     @PostMapping
-    public ResponseEntity<BookResponseDTO> insert(@RequestBody BookRequestDTO dto){
+    public ResponseEntity<BookResponseDTO> insert(@RequestBody BookRequestDTO dto) {
 
         BookResponseDTO book = service.insertNewBook(dto);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(book.getId()).toUri();
 
         return ResponseEntity.created(uri).body(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable UUID id){
+
+        service.deleteBookById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/caue/bookstore/services/BookService.java
+++ src/main/java/com/caue/bookstore/services/BookService.java
@@ -1,20 +1,22 @@
 package com.caue.bookstore.services;
 
-import com.caue.bookstore.dto.AuthorDTO;
+import com.caue.bookstore.AIService.AssistantService;
 import com.caue.bookstore.dto.BookRequestDTO;
 import com.caue.bookstore.dto.BookResponseDTO;
-import com.caue.bookstore.dto.CategoryDTO;
 import com.caue.bookstore.entities.Author;
 import com.caue.bookstore.entities.Book;
 import com.caue.bookstore.entities.Category;
-import com.caue.bookstore.exceptions.ResourceNotFoundException;
 import com.caue.bookstore.projections.BookProjection;
 import com.caue.bookstore.repositories.AuthorRepository;
 import com.caue.bookstore.repositories.BookRepository;
 import com.caue.bookstore.repositories.CategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,12 +26,13 @@ public class BookService {
     private final BookRepository repository;
     private final CategoryRepository categoryRepository;
     private final AuthorRepository authorRepository;
+    private final AssistantService assistantService;
 
-
-    public BookService(BookRepository repository, CategoryRepository categoryRepository, AuthorRepository authorRepository) {
+    public BookService(BookRepository repository, CategoryRepository categoryRepository, AuthorRepository authorRepository, AssistantService assistantService) {
         this.repository = repository;
         this.categoryRepository = categoryRepository;
         this.authorRepository = authorRepository;
+        this.assistantService = assistantService;
     }
 
     @Transactional(readOnly = true)
@@ -40,17 +43,27 @@ public class BookService {
         return new BookResponseDTO(entity);
     }
 
+    @Transactional(readOnly = true)
+    public Page<BookResponseDTO> getAllBooks(@RequestParam Pageable pageable) {
+
+        return repository.findAll(pageable).map(BookResponseDTO::new);
+    }
+
     @Transactional
     public BookResponseDTO insertNewBook(BookRequestDTO dto) {
 
         Book entity = new Book();
 
+        BookRequestDTO bookRequestDTO = assistantService.extractBookData(dto.getTitle());
+
         entity.setTitle(dto.getTitle());
-        entity.setIsbn(dto.getIsbn());
-        entity.setReleaseDate(dto.getReleaseDate());
+        entity.setIsbn(bookRequestDTO.getIsbn());
+        entity.setReleaseDate(bookRequestDTO.getReleaseDate());
         entity.setStock(dto.getStock());
         entity.setPrice(dto.getPrice());
-        entity.setDescription(dto.getDescription());
+        entity.setCoverImageUrl(dto.getCoverImageUrl());
+
+        entity.setDescription(bookRequestDTO.getDescription());
 
         List<Category> categories = categoryRepository.findAllById(dto.getCategoriesIds());
 
@@ -62,5 +75,10 @@ public class BookService {
         entity = repository.saveAndFlush(entity);
 
         return new BookResponseDTO(entity);
+    }
+
+    public void deleteBookById(UUID id) {
+
+        repository.deleteById(id);
     }
 }
